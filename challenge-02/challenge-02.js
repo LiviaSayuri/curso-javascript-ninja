e8e3597cb1c26539f38c9fd03eb1c14521e2e662
Livia Sayuri Souza <livia.souza@easynvest.com.br>
2018-10-09 15:13:43 -0300

    Resolução Desafio 2

---
+++ challenge-02/challenge-02.js
@@ -0,0 +1,100 @@
+// Crie uma função que receba dois argumentos e retorne a soma dos mesmos.
+function soma(a, b) {
+    return a + b;
+}
+
+// Declare uma variável que receba a invocação da função criada acima, passando dois números quaisquer por argumento, e somando `5` ao resultado retornado da função.
+var resultado;
+resultado = soma(10, 1) + 5;
+
+// Qual o valor atualizado dessa variável 
+console.log(resultado);
+
+// Declare uma nova variável, sem valor.
+var nova;
+console.log(nova);
+/*
+Crie uma função que adicione um valor à variável criada acima, e retorne a string:
+    O valor da variável agora é VALOR.
+Onde VALOR é o novo valor da variável.
+*/
+function adicionaValor() {
+    nova = 10;
+    return console.log("O valor da variável agora é " + nova);
+}
+
+// Invoque a função criada acima.
+adicionaValor();
+
+
+// Qual o retorno da função  (Use comentários de bloco).
+console.log(nova);
+/*Retornou "O valor da variável agora é 10"*/
+
+/*
+Crie uma função com as seguintes características:
+1. A função deve receber 3 argumentos;
+2. Se qualquer um dos três argumentos não estiverem preenchidos, a função deve retornar a string:
+    Preencha todos os valores corretamente!
+3. O retorno da função deve ser a multiplicação dos 3 argumentos, somando `2` ao resultado da multiplicação.
+*/
+function multiplicacaoCompleta(a, b, c) {
+    if (a === undefined || b === undefined || c === undefined) {
+        return "Preencha todos os valores corretamente!";
+    }
+    else {
+        return (a * b * c) + 2;
+    }
+}
+
+// Invoque a função criada acima, passando só dois números como argumento.
+console.log(multiplicacaoCompleta(10, 2));
+
+// Qual o resultado da invocação acima  (Use comentários para mostrar o valor retornado).
+//Preencha todos os valores corretamente!
+
+// Agora invoque novamente a função criada acima, mas passando todos os três argumentos necessários.
+
+
+// Qual o resultado da invocação acima  (Use comentários para mostrar o valor retornado).
+console.log(multiplicacaoCompleta(10, 2,3));
+
+/*
+Crie uma função com as seguintes características:
+1. A função deve receber 3 argumentos.
+2. Se somente um argumento for passado, retorne o valor do argumento.
+3. Se dois argumentos forem passados, retorne a soma dos dois argumentos.
+4. Se todos os argumentos forem passados, retorne a soma do primeiro com o segundo, e o resultado, dividido pelo terceiro.
+5. Se nenhum argumento for passado, retorne o valor booleano `false`.
+6. E ainda, se nenhuma das condições acima forem atendidas, retorne `null`.
+*/
+function dependeDosArgumentos(a,b,c)
+{
+    if (a === undefined)
+    {
+        return false;
+    }
+    else if (b === undefined)
+    {
+        return a;
+    }
+    else if (c === undefined)
+    {
+        return a+b;
+    }
+    else if (a !== NaN && b !== NaN && c !== NaN){
+        return (a+b)/c;
+    }
+    else{
+        return null;
+    }
+}
+// Invoque a função acima utilizando todas as possibilidades (com nenhum argumento, com um, com dois e com três.) Coloque um comentário de linha ao lado da função com o resultado de cada invocação.
+//Nenhum
+console.log(dependeDosArgumentos());
+//Um
+console.log(dependeDosArgumentos(10));
+//Dois
+console.log(dependeDosArgumentos(10,2));
+//Três
+console.log(dependeDosArgumentos(10,2,3));
